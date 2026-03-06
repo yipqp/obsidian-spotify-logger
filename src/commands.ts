b5d6e9e00e89e1ba861f582f5f6162fe5bb3751a
yipqp <114359598+yipqp@users.noreply.github.com>
2026-03-06 01:40:06 -0800

    Use checkCallback in commands

--- src/commands.ts
+++ src/commands.ts
@@ -5,12 +5,13 @@ import {
 	getAuthUrl,
 	getCurrentlyPlayingTrack,
 	getRecentlyPlayed,
+	isAuthenticated,
 	processCurrentlyPlayingResponse,
 	processRecentlyPlayed,
 } from "./api";
 import { ItemFormatted, ItemType } from "types";
 import { RecentSongsModal } from "./ui/RecentSongsModal";
-import { requireAuth, showNotice } from "./utils";
+import { showNotice } from "./utils";
 import Scrobble from "./main";
 
 export function registerCommands(plugin: Scrobble) {
@@ -67,17 +68,31 @@ export function registerCommands(plugin: Scrobble) {
 	plugin.addCommand({
 		id: "scrobble-currently-playing-track",
 		name: "Scrobble currently playing song",
-		callback: requireAuth(plugin.app, async () => {
-			await scrobbleCurrentlyPlaying("Track");
-		}),
+		checkCallback: (checking: boolean) => {
+			if (isAuthenticated(plugin.app)) {
+				if (!checking) {
+					void scrobbleCurrentlyPlaying("Track");
+				}
+				return true;
+			}
+			showNotice("Please connect your Spotify account", true);
+			return false;
+		},
 	});
 
 	plugin.addCommand({
 		id: "scrobble-currently-playing-album",
 		name: "Scrobble currently playing album",
-		callback: requireAuth(plugin.app, async () => {
-			await scrobbleCurrentlyPlaying("Album");
-		}),
+		checkCallback: (checking: boolean) => {
+			if (isAuthenticated(plugin.app)) {
+				if (!checking) {
+					void scrobbleCurrentlyPlaying("Album");
+				}
+				return true;
+			}
+			showNotice("Please connect your Spotify account", true);
+			return false;
+		},
 	});
 
 	plugin.addCommand({
@@ -92,32 +107,68 @@ export function registerCommands(plugin: Scrobble) {
 	plugin.addCommand({
 		id: "search-track",
 		name: "Search songs",
-		callback: requireAuth(plugin.app, async () => {
-			new SearchModal(plugin.app, "Track", scrobbleSearchedSong).open();
-		}),
+		checkCallback: (checking: boolean) => {
+			if (isAuthenticated(plugin.app)) {
+				if (!checking) {
+					new SearchModal(
+						plugin.app,
+						"Track",
+						scrobbleSearchedSong,
+					).open();
+				}
+				return true;
+			}
+			showNotice("Please connect your Spotify account", true);
+			return false;
+		},
 	});
 
 	plugin.addCommand({
 		id: "search-album",
 		name: "Search albums",
-		callback: requireAuth(plugin.app, async () => {
-			new SearchModal(plugin.app, "Album", scrobbleSearchedSong).open();
-		}),
+		checkCallback: (checking: boolean) => {
+			if (isAuthenticated(plugin.app)) {
+				if (!checking) {
+					new SearchModal(
+						plugin.app,
+						"Album",
+						scrobbleSearchedSong,
+					).open();
+				}
+				return true;
+			}
+			showNotice("Please connect your Spotify account", true);
+			return false;
+		},
 	});
 
 	plugin.addCommand({
 		id: "search-recent-tracks",
 		name: "Search recent songs",
-		callback: requireAuth(plugin.app, async () => {
-			// TODO: change requireAuth callbacks ..
-			const recentlyPlayed = await getRecentlyPlayed(plugin.app);
-			const recentlyPlayedFormatted =
-				processRecentlyPlayed(recentlyPlayed);
-			new RecentSongsModal(
-				plugin.app,
-				recentlyPlayedFormatted,
-				scrobbleSearchedSong,
-			).open();
-		}),
+		checkCallback: (checking: boolean) => {
+			if (isAuthenticated(plugin.app)) {
+				if (!checking) {
+					void (async () => {
+						try {
+							const recentlyPlayed = await getRecentlyPlayed(
+								plugin.app,
+							);
+							const recentlyPlayedFormatted =
+								processRecentlyPlayed(recentlyPlayed);
+							new RecentSongsModal(
+								plugin.app,
+								recentlyPlayedFormatted,
+								scrobbleSearchedSong,
+							).open();
+						} catch (err) {
+							showNotice(err.message, true);
+						}
+					})();
+				}
+				return true;
+			}
+			showNotice("Please connect your Spotify account", true);
+			return false;
+		},
 	});
 }
